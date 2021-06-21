07e22bf9ab37658e076edb3043f04e425d930148
schueler <schueler@ubuntu>
2021-06-21 14:30:26 +0200

    Main changes

--- MyTeam/src/pkgMain/Main.java
+++ MyTeam/src/pkgMain/Main.java
@@ -7,9 +7,11 @@ public class Main {
 	public static void main(String[] args) {
 		Player player = new Player(2, "Dragovic", "Defense");
 		System.out.println("== " + player);
+		
 		player = new Player(4, "Hinteregger", "Defense");
 		System.out.println("==" + player);
-		player = new Player(5, "Alaba", "Defense");
+		
+		player = new Player(5, "Sabizer", "Attack");
 		System.out.println("==" + player);
 	}
 
